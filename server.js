79eef94c58a72578f3c5a03286eb37f19a12b425
Umar_Aliy <umaraliy092@gmail.com>
2024-05-17 05:32:05 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web Serverni Boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1 Kirish code
 app.use(express.static("public"));
@@ -18,6 +28,10 @@ app.set("view engine", "ejs");
 app.post("/create-item", (req, res) => {
   console.log(req);
   res.json({ test: "success" });
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 app.get("/", function (req, res) {
   res.render("harid");
--- train.js
+++ train.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 console.log("train task ishga tushdi!");
 
 // D-TASK
-=======
-console.log("train task ishga tushdi!");
-
-// D-TASK
->>>>>>> ab431d21f26f66fa5a89c2e5988aaef3887d7550
